b54fb08cce97718a74da1ab7758263f7fdb0a14d
Rajeeva Acharya <rajeevaa@amazon.com>
2017-08-23 22:49:53 -0700

    namovrige placeholder :no_mouth:

--- ui/src/js/component/family/index.jsx
+++ ui/src/js/component/family/index.jsx
@@ -5,24 +5,6 @@ import Pic from 'element/pic';
 
 export default class Family extends React.Component {
 
-  getPanel() {
-    return(
-      <Panel>
-        {this.getRow()}
-      </Panel>
-    );
-  }
-
-  getRow() {
-    const arr = [1,2,3];
-    const listItems = arr.map((number) => this.getPic());
-    return(
-      <Row>
-        {listItems}
-      </Row>
-    );
-  }
-
   getPic() {
     const src = "https://lh4.googleusercontent.com/-N_O16AzbOqk/AAAAAAAAAAI/AAAAAAAAAAA/fkDRL0S6mE4/s128-c-k/photo.jpg";
     return (
@@ -32,12 +14,80 @@ export default class Family extends React.Component {
     );
   }
 
-  render() {
+  showParents() {
+    const src = "https://lh4.googleusercontent.com/-N_O16AzbOqk/AAAAAAAAAAI/AAAAAAAAAAA/fkDRL0S6mE4/s128-c-k/photo.jpg";
+    const parents = (
+      <strong>Parents</strong>
+    );
+    const children = (
+      <strong>Children</strong>
+    );
     const arr = [1,2,3];
-    const listItems = arr.map((number) => this.getPanel());
+    const listItems = arr.map((number) => this.getPic());
+    return (
+      <Row>
+        <Col xs={6}>
+          <Panel header={parents}>
+            <Col xs={6}>
+              <Pic src={src} />
+            </Col>
+            <Col>
+              <Pic src={src} />
+            </Col>
+          </Panel>
+        </Col>
+        <Col xs={6}>
+          <Panel header={children}>
+            {listItems}
+          </Panel>
+        </Col>
+      </Row>
+    );
+  }
+
+  showPerson() {
+    const src = "https://lh4.googleusercontent.com/-N_O16AzbOqk/AAAAAAAAAAI/AAAAAAAAAAA/fkDRL0S6mE4/s128-c-k/photo.jpg";
+    return (
+       <Row>
+        <Col xs={6}>
+          <Panel header="Husband">
+            <Pic src={src} />
+          </Panel>
+        </Col>
+        <Col xs={6}>
+          <Panel header="Wife">
+            <Pic src={src} />
+          </Panel>
+        </Col>
+      </Row>
+    );
+  }
+
+  showSiblings() {
+    const src = "https://lh4.googleusercontent.com/-N_O16AzbOqk/AAAAAAAAAAI/AAAAAAAAAAA/fkDRL0S6mE4/s128-c-k/photo.jpg";
+    const arr = [1,2,3];
+    const listItems = arr.map((number) => this.getPic());
+    return (
+      <Row>
+        <Col xs={6}>
+          <Panel header="Siblings">
+            <Pic src={src} />
+          </Panel>
+        </Col>
+        <Col xs={6}>
+          <Panel header="Cousins">
+            {listItems}
+          </Panel>
+        </Col>
+      </Row>
+    );
+  }
+  render() {
     return (
       <Grid>
-        {listItems}
+        {this.showParents()}
+        {this.showPerson()}
+        {this.showSiblings()}
       </Grid>
     );
   }
